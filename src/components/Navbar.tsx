5e870e41df321c7d6756d65d8aa0920b98b833e8
Jaden-77 <131693026+Jaden-77@users.noreply.github.com>
2024-05-28 14:25:35 +0800

    1

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Navbar = () => {
+  <nav
+    className="sticky z-[100] h-14 inset x-0 top-0 w-full border-b
+    border-gray-200 bg-white/75 backdrop-blur-lg transition-all"
+  >
+    <MaxWidthWrapper>
+        <div></div>
+    </MaxWidthWrapper>
+  </nav>;
+};
+
+export default Navbar;
